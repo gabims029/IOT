e1e32b10436b7ef021dbfd9121874ec6ff9100dc
gabims029 <gabimelo541@gmail.com>
2025-09-15 09:02:34 -0300

    08/09

--- CalculoTemperatura/subscribe.ino
+++ CalculoTemperatura/subscribe.ino
@@ -1,32 +1,24 @@
-
 // Função chamada automaticamente quando o feed receber atualizações
-void handleTemperatura(AdafruitIO_Data *data){
-  float temp = data -> toFloat();
+void handleTemperatura(AdafruitIO_Data *data) {
+  float temp = data->toFloat();
 
   Serial.print(F("Temperatura do feed: "));
   Serial.print(temp, 2);
   Serial.println(F("°C"));
 
-  // Lógica para acender ou não o LED
-  if(temp <= 0){
-    digitalWrite(pinLed, HIGH);
-  }
-  else{
-    digitalWrite(pinLed, LOW);
-  }
+  //lógica para acender ou não o led
+  // if(temp < 0){
+  //   digitalWrite(pinLed, HIGH);
+  // }
+  // else{
+  //   digitalWrite(pinLed, LOW);
+  // }
 }
 
-void handleBotaoLed(AdafruitIO_Data *data){
-  String state = data->toString();
-
-
-  Serial.print(F("State do feed: "));
-  Serial.println(state);
-
-  if(state == "true"){
-    digitalWrite(pinLed, HIGH);
-  }
-  else{
-    digitalWrite(pinLed, LOW);
-  }
+void handleBotaoLed(AdafruitIO_Data *data) {
+  // if (!data->toBool()) {
+  //   digitalWrite(pinLed, HIGH);
+  // } else {
+  //   digitalWrite(pinLed, LOW);
+  // }
 }
